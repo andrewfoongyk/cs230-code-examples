Added GP inference, fixed a bug in HMC that used the wrong prior for the biases - I was previously scaling the bias prior in the same way as the weight priors

pytorch/vision/build_1d_dataset.py
@@ -49,13 +49,16 @@ class Prior_Dataset_Builder(object):
         plt.show()         
 
 if __name__ == '__main__':
-    n = 100 # number of datapoints
+    n = 2 # number of datapoints
     noise_var = 0.01 # datapoint noise
     X = np.random.uniform(-1, 1, n)
+    # X1 = np.random.uniform(-1, -0.7, 50)
+    # X2 = np.random.uniform(0.5, 1, 50)
+    # X = np.concatenate((X1, X2), axis=0)
 
-    cosx = np.cos(4*X + 0.8)
+    # cosx = np.cos(4*X + 0.8)
     randn = np.random.randn(n)*np.sqrt(noise_var)
-    Y = cosx + randn
+    Y = 0.5*X + 0.5 + randn
 
     # def func(x):
     #     return np.cos(2*x + 0.8)
@@ -73,16 +76,16 @@ if __name__ == '__main__':
     data = np.c_[X.reshape(len(X), -1), Y.reshape(len(Y), -1)]
 
     # pickle the dataset 
-    with open('data\\1D_COSINE\\1d_cosine_compressed.pkl', 'wb') as f:
+    with open('data//1D_COSINE//1d_cosine_separated.pkl', 'wb') as f:
         pickle.dump(data, f)
 
     # unpickle the dataset
-    with open('data\\1D_COSINE\\1d_cosine_compressed.pkl', 'rb') as f:
-        data = pickle.load(f)
+    with open('data//1D_COSINE//1d_cosine_separated.pkl', 'rb') as f:
+        data_load = pickle.load(f)
 
     # plot the dataset
     plt.figure()
-    plt.plot(data[:,0], data[:,1], '+k')
+    plt.plot(data_load[:,0], data_load[:,1], '+k')
     plt.show()
 
 

pytorch/vision/model/net.py
@@ -12,7 +12,10 @@ import math
 
 # activation functions
 def bump(x):
-    return torch.exp(-x**2)
+    return torch.exp(-x**2)*F.relu(2-torch.abs(x)) # truncated bump (untrainable?)
+
+def retanh(x):
+    return torch.tanh(x)*F.relu(x)
 
 class FCVI_Net(nn.Module):
     def __init__(self, params, prior_init=False): 
@@ -61,8 +64,9 @@ class FCVI_Net(nn.Module):
 
     def forward(self, x, no_samples, shared_weights=False): 
         batch_size = x.size()[0] # x has dimensions (batch_size x no_inputs)
+        # initialise empty cholesky matrix
+        L_init = torch.cuda.FloatTensor(self.no_params, self.no_params).fill_(0)
         # use cov_vector to fill in L
-        L_init = torch.zeros(self.no_params, self.no_params).cuda()
         L_init[np.tril_indices(self.no_params)] = self.cov_vector
         # exponentiate the diagonal
         L_diag = torch.diag(L_init)
@@ -171,7 +175,6 @@ class FCVI_Net(nn.Module):
         layer_weights = []
         layer_biases = []
         end_index = end_index + self.hidden_sizes[-1]*self.output_size
-        #print('HEREREEEE')
         
         weight_mean_vector = self.mean[start_index:end_index]
         weight_mean_matrix = weight_mean_vector.view(self.hidden_sizes[-1], self.output_size)
@@ -231,11 +234,11 @@ class FCVI_Net(nn.Module):
 
     def get_samples(self, L, no_samples, batch_size, shared_weights=False): # return samples of all the parameters, (no_params x no_samples x batch_size)
         if shared_weights == True:
-            z = Variable(torch.Tensor(self.no_params, no_samples).normal_(0, 1).cuda())
+            z = Variable(torch.cuda.FloatTensor(self.no_params, no_samples).normal_(0, 1))
             z = z.expand(batch_size, -1, -1)
             z = z.permute(1,2,0)
         else:
-            z = Variable(torch.Tensor(self.no_params, no_samples, batch_size).normal_(0, 1).cuda())
+            z = Variable(torch.cuda.FloatTensor(self.no_params, no_samples, batch_size).normal_(0, 1))
         means = self.mean.expand(no_samples, batch_size, -1)
         means = means.permute(2,0,1)
         params_samples = means + torch.einsum('ab,bcd->acd', (L, z)) ######## check this
@@ -992,14 +995,14 @@ class MFVI_Linear_Layer(nn.Module):
         # prior parameters 
         self.W_prior_mean = Variable(torch.zeros(n_input, n_output).cuda())
         self.W_prior_logvar = Variable((prior_logvar*torch.ones(n_input, n_output)).cuda())
-        self.b_prior_mean = Variable(torch.zeros(n_output).cuda())
-        self.b_prior_logvar = Variable((prior_logvar*torch.ones(n_output)).cuda())
+        self.b_prior_mean = Variable(torch.zeros(n_output).cuda()) 
+        self.b_prior_logvar = Variable((torch.zeros(n_output)).cuda())# prior logvar on biases unaffected by Neal's scaling
 
         if prior_init == True: # initialise parameters to their prior values
             self.W_mean = nn.Parameter(torch.zeros(n_input, n_output))
             self.W_logvar = nn.Parameter(prior_logvar*torch.ones(n_input, n_output))
-            self.b_mean = nn.Parameter(torch.zeros(n_output))
-            self.b_logvar = nn.Parameter(prior_logvar*torch.ones(n_output))
+            self.b_mean = nn.Parameter(torch.zeros(n_output)) 
+            self.b_logvar = nn.Parameter(torch.zeros(n_output)) # prior logvar on biases unaffected by Neal's scaling
 
         self.num_weights = n_input*n_output + n_output # number of weights and biases
  
@@ -1015,8 +1018,8 @@ class MFVI_Linear_Layer(nn.Module):
             # sample just one weight matrix and just one bias vector
             W_var = torch.exp(self.W_logvar)
             b_var = torch.exp(self.b_logvar)
-            z_W = Variable(torch.Tensor(self.n_input, self.n_output).normal_(0, 1).cuda())
-            z_b = Variable(torch.Tensor(self.n_output).normal_(0, 1).cuda())
+            z_W = Variable(torch.cuda.FloatTensor(self.n_input, self.n_output).normal_(0, 1))
+            z_b = Variable(torch.cuda.FloatTensor(self.n_output).normal_(0, 1))
             W = self.W_mean + torch.mul(torch.sqrt(W_var), z_W)
             b = self.b_mean + torch.mul(torch.sqrt(b_var), z_b)
             b = b.expand(batch_size, -1)
@@ -1051,12 +1054,12 @@ class MFVI_Linear_Layer(nn.Module):
         return samples_activations
 
     def get_shared_random(self, no_samples, batch_size):
-        z = Variable(torch.Tensor(no_samples, self.n_output).normal_(0, 1).cuda())
+        z = Variable(torch.cuda.FloatTensor(no_samples, self.n_output).normal_(0, 1))
         z = z.expand(batch_size, -1, -1)
         return torch.transpose(z, 0, 1)
 
     def get_random(self, no_samples, batch_size):
-        return Variable(torch.Tensor(no_samples, batch_size, self.n_output).normal_(0, 1).cuda()) # standard normal noise matrix
+        return Variable(torch.cuda.FloatTensor(no_samples, batch_size, self.n_output).normal_(0, 1)) # standard normal noise matrix
 
     def KL(self): # get KL between q and prior for this layer
         # W_KL = 0.5*(- self.W_logvar + torch.exp(self.W_logvar) + (self.W_mean)**2)
@@ -1125,6 +1128,24 @@ class MFVI_Net(nn.Module):
                     s = self.activation(s, self.prelu_weight)
                 else:
                     s = self.activation(s) 
+                ##################################### THIS ISN'T WORKING - TRY A MASK INSTEAD
+                #############################################################################
+                # s_bump = bump(s)
+                # no_dims = len(s.size())
+
+                # no_bump_units = int(s_bump.size()[-1]/2) 
+                # s_retanh = bump(s)
+
+                # if no_dims == 2:
+                #     s = torch
+                #     s[:, :no_bump_units] = s_bump[:, :no_bump_units]
+                #     s[:, no_bump_units:] = s_retanh[:, no_bump_units:]
+                # elif no_dims == 3:
+                #     s[:, :, :no_bump_units] = s_bump[:, :,no_bump_units]
+                #     s[:, :, no_bump_units:] = s_retanh[:, :, no_bump_units:]
+                # else: 
+                #     print(no_dims)
+                #     print('errrrrrrrrrrrrrrrrrrrrrrrror')
         if self.dataset == '1d_cosine' or self.dataset == 'prior_dataset': # make this more flexible
             # s has dimension (no_samples x batch_size x no_output=1)
             s = s.view(no_samples, -1) # (no_samples x batch_size)

pytorch/vision/train.py
@@ -102,21 +102,14 @@ class Weight_Plot_Saver(object):
                 self.x_weights = np.linspace(-4, 4, 1000) # since bias variances do not scale with size of input to layer
 
         if parameter == 'weights':
-            # print('starting the weights')
             # plot all the weights in units
             for i, u in enumerate(units):
-                # print(len(weights))
-                # print('layer:{}'.format(u[0]))
-                # print('row:{}'.format(u[1]))
-                # print('column:{}'.format(u[2]))
-                # print(weights[u[0]][0].shape)
                 mu = weights[u[0]][0][u[1]][u[2]] # [layer][mean/sd][weightrow][weightcol]
                 sigma = weights[u[0]][1][u[1]][u[2]]
                 y_weights = scipy.stats.norm.pdf(self.x_weights, mu, sigma)
                 self.axis_weights.plot(self.x_weights, y_weights, self.styles[i%6], linewidth=0.3)
             # print('did the weights')
         elif parameter == 'biases':
-            # print('starting the biases')
             # plot all the biases in units
             for i, u in enumerate(units):
                 mu = biases[u[0]][0][u[1]] # [layer][mean/sd][biasrow]
@@ -124,8 +117,16 @@ class Weight_Plot_Saver(object):
                 y_weights = scipy.stats.norm.pdf(self.x_weights, mu, sigma)
                 self.axis_weights.plot(self.x_weights, y_weights, self.styles[i%6], linewidth=0.3)
             # print('did the biases')
-        elif parameter == 'covariance': # for FCVI
-            self.axis_weights.imshow(np.abs(model.Sigma.cpu().detach().numpy()), interpolation='nearest', cmap=cm.Greys_r)
+        elif parameter == 'covariance': # for FCVI 
+            Sigma = model.Sigma.cpu().detach().numpy()          
+            self.axis_weights.imshow(np.abs(Sigma), interpolation='nearest', cmap=cm.Greys_r)
+        elif parameter == 'correlation': # for FCVI
+            Sigma = model.Sigma.cpu().detach().numpy()
+            Sigma_diag = np.diag(Sigma)
+            outer = np.outer(Sigma_diag, Sigma_diag)
+            Correlations = Sigma/np.sqrt(outer)         
+            im = self.axis_weights.imshow(Correlations , interpolation='nearest')
+            self.fig_weights.colorbar(im)
         elif parameter == 'mean': # for FCVI
             self.axis_weights.imshow(model.mean.cpu().detach().numpy(), interpolation='nearest', cmap=cm.Greys_r)
 
@@ -234,13 +235,16 @@ def whole_dataset_train(model, optimizer, loss_fn, dataloader, metrics, params, 
     
     # make list of units to monitor
     no_units = params.hidden_sizes[0] ########################## change
+    no_output_units = params.hidden_sizes[-1]
     input_units = []
     output_units = []
     bias_units = []
     for i in range(no_units):
         input_units.append((0, 0, i))
-        output_units.append((len(params.hidden_sizes), i, 0)) # change depending on no. of layers
         bias_units.append((0,i))
+
+    for i in range(no_output_units):
+        output_units.append((len(params.hidden_sizes), i, 0)) # change depending on no. of layers
 
     # plotter = Weight_Plotter(model, units = units) # initialise weight animation plotter
     weight_plot_save = Weight_Plot_Saver(args.model_dir)
@@ -311,6 +315,7 @@ def whole_dataset_train(model, optimizer, loss_fn, dataloader, metrics, params, 
                 weight_plot_save.save(model, epoch=i, units=bias_units, name=label + 'biases', parameter='biases')
                 if params.model == 'fcvi':
                     weight_plot_save.save(model, epoch=i, units=None, name='covariance', parameter='covariance', plot_prior=False)
+                    weight_plot_save.save(model, epoch=i, units=None, name='correlation', parameter='correlation', plot_prior=False)
                     #weight_plot_save.save(model, epoch=i, units=None, name='mean', parameter='mean', plot_prior=False)
 
             if i == no_epochs-1:
@@ -319,6 +324,7 @@ def whole_dataset_train(model, optimizer, loss_fn, dataloader, metrics, params, 
                 weight_plot_save.save(model, epoch=i, units=bias_units, name=label + 'biases', parameter='biases')
                 if params.model == 'fcvi':
                     weight_plot_save.save(model, epoch=i, units=None, name='covariance', parameter='covariance', plot_prior=False)
+                    weight_plot_save.save(model, epoch=i, units=None, name='correlation', parameter='correlation', plot_prior=False)
                     #weight_plot_save.save(model, epoch=i, units=None, name='mean', parameter='mean', plot_prior=False)
 
             # update the average loss
@@ -563,7 +569,7 @@ if __name__ == '__main__':
     #plt.show()
     # plot results for regression task
     plot_reg(model, args.data_dir, params, args.model_dir, epoch_number=params.num_epochs)
-    weight_plot_save.save(model, epoch=params.num_epochs)
+    # weight_plot_save.save(model, epoch=params.num_epochs)
 
     # pr.disable()
     # pr.print_stats()